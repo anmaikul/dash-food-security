d7d30fec96532e63b945938eb551166fbf942d24
michaelan0925@gmail.com <michaelan0925@gmail.com>
2019-06-15 17:07:46 -0400

    basic plotly tutorial working app

--- app.py
+++ app.py
@@ -5,66 +5,68 @@ import pandas as pd
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 
-app = dash.Dash()
+external_stylesheets = ['styles.css']
 
-
-app.layout = html.Div([
-    dcc.Input(id='my-id', value='Dash App', type='text'),
-    html.Div(id='my-div')
-])
-
-@app.callback(
-    Output(component_id='my-div', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
-)
-def update_output_div(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
-
-
-# df = pd.read_csv(
-#     'https://gist.githubusercontent.com/chriddyp/' +
-#     '5d1ea79569ed194d432e56108a04d188/raw/' +
-#     'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
-#     'gdp-life-exp-2007.csv')
+app = dash.Dash(external_stylesheets=external_stylesheets)
 
 
 # app.layout = html.Div([
-#     html.Label('Multi-Select Dropdown', style={'backgroundColor': 'red', 'textAlign': 'center',}),
-#     dcc.RadioItems(
-#         options=[
-#             {'label': 'New York City', 'value': 'NYC'},
-#             {'label': u'Montréal', 'value': 'MTL'},
-#             {'label': 'San Francisco', 'value': 'SF'}
-#         ],
-#         value='MTL',
-#     ),
-#     dcc.Graph(
-#         id='life-exp-vs-gdp',
-#         figure={
-#             'data': [
-#                 go.Scatter(
-#                     x=df[df['continent'] == i]['gdp per capita'],
-#                     y=df[df['continent'] == i]['life expectancy'],
-#                     text=df[df['continent'] == i]['country'],
-#                     mode='markers',
-#                     opacity=0.8,
-#                     marker={
-#                         'size': 15,
-#                         'line': {'width': 0.5, 'color': 'white'}
-#                     },
-#                     name=i
-#                 ) for i in df.continent.unique()
-#             ],
-#             'layout': go.Layout(
-#                 xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-#                 yaxis={'title': 'Life Expectancy'},
-#                 margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-#                 legend={'x': 0, 'y': 1},
-#                 hovermode='closest'
-#             )
-#         }
-#     )
+#     dcc.Input(id='my-id', value='Dash App', type='text'),
+#     html.Div(id='my-div')
 # ])
+
+# @app.callback(
+#     Output(component_id='my-div', component_property='children'),
+#     [Input(component_id='my-id', component_property='value')]
+# )
+# def update_output_div(input_value):
+#     return 'You\'ve entered "{}"'.format(input_value)
+
+
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
+
+
+app.layout = html.Div([
+    html.Label('Multi-Select Dropdown', id='test-label-id', style={'textAlign': 'center',}),
+    dcc.RadioItems(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='MTL',
+    ),
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.8,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
 
 
 
